43038d5eb9d0386a24cde47f5f188df0a9e76cc7
i-Apoorva <apoorva.ambhoj95@gmail.com>
2020-04-15 02:38:57 +0530

    test deploy

--- src/app.js
+++ src/app.js
@@ -23,14 +23,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Apoorva Ambhoj'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Apoorva Ambhoj'
     })
 })
 
@@ -38,7 +38,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Apoorva Ambhoj'
     })
 })
 
@@ -63,17 +63,12 @@ app.get('/weather', (req, res) => {
             })
         })
     }
-    // console.log(req.query)
-    // res.json({
-    //     forecast: 'It is snowing',
-    //     location: req.query.address
-    // })
 })
 
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Apoorva Ambhoj',
         errorMessage: 'Help article not found.'
     })
 })
@@ -81,7 +76,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Apoorva Ambhoj',
         errorMessage: 'Page not found.'
     })
 })
